12fdef1b56025b21805568b67c8e7e27d72c21ea
carlos-martinez-maqueta <cmartinez.meneses1@gmail.com>
2024-09-06 21:38:24 -0500

    WEB

---
+++ componentes/head.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Canastas Navideñas 2024 | Canastas de Navidad Lima, Perú</title>
+    <meta name="description" content="Canastas Navideñas, esta navidad 2024 ofrecemos canastas navideñas corporativas con viveres, para ejecutivos. En diferentes envases y panetones de las mejores marcas.">
+    
+    <link rel="apple-touch-icon" href="assets/img/apple-icon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/templatemo.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+
+    <!-- Load fonts style after rendering the layout styles -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+
+</head>
